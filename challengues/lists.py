ab8c851241963ba7a6473be22c7c4f6b734e1afe
alexisraelmtz <alex.israel.mtz@gmail.com>
2020-07-17 17:17:59 -0500

    New Challenge with Iterations (Unfinished)

---
+++ challengues/lists.py
@@ -0,0 +1,31 @@
+n = int(input("Type the number of commands> "))
+text = input("Type the operation> ").split()
+
+
+def array_addition():
+    array = []
+    
+    for i in range(n):
+        if text[i] == "intesert"
+
+
+        else
+            print("Command not detected. Type Help for list of commands.")
+    
+    print(total)
+
+
+array_addition()
+
+
+"""
+
+1. insert i e: Insert integer at position
+2. print: Print the list.
+3. remove e: Delete the first occurrence of integer
+4. append e: Insert integer at the end of the list.
+5. sort: Sort the list.
+6. pop: Pop the last element from the list.
+7. reverse: Reverse the list.
+
+"""

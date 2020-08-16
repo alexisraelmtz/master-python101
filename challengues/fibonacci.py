a7cca0ac37bc7b7fbc07b290c72f38cf9b2d98a0
alexisraelmtz <alex.israel.mtz@gmail.com>
2020-08-16 11:24:19 -0400

    Updates to Fibonacci Function

--- challengues/fibonacci.py
+++ challengues/fibonacci.py
@@ -1,24 +1,33 @@
-# answer = list(map(int, input().split()))
-# start = answer[0]
-# length = answer[1]
-
-start = 1
-length = 10
-
-
-#def Fibo(start, length):
-#    a, b = (start - 1), (start)
-#    for _ in range(length):
-#        print(f"{a}")
-#        a, b = b, a + b
-
-#    return a
+answer = list(
+    map(
+        int, input("Place a number to start, and the lenght of the succesion: ").split()
+    )
+)
+start = answer[0]
+length = answer[1]
 
 
-#succesion = Fibo(start, length)
-#print(succesion)
+def Fibo(start, length):
+    succesion = []
+    if start == 0:
+        a, b = (start), (1)
+    elif start <= 1:
+        a, b = (start - 1), (start)
 
-a,b = (start - 1), (start)
-a, b = b, a + b
-succesion = [(a for a,b in range(start:length)]
+    for _ in range(length):
+        succesion.append(a)
+        a, b = b, a + b
+    return succesion
+
+
+succesion = Fibo(start, length)
 print(succesion)
+
+
+# start = 1
+# length = 10
+
+# a, b = (start - 1), (start)
+# for _ in range(length):
+#     print(f"{a}")
+#     a, b = b, a + b

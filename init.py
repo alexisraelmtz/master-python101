f4502ecee130541d23ebaff01c64b540e81c159d
alexisraelmtz <alex.israel.mtz@gmail.com>
2021-04-06 22:41:35 -0500

    test py. file on new setup

---
+++ init.py
@@ -0,0 +1,16 @@
+import sys
+
+# import pandas
+
+# print(sys.version)
+# print(sys.executable)
+
+
+def hi(who_is):
+    hello = f"Hello {who_is}"
+    print(sys.version)
+    print(sys.executable)
+    return hello
+
+
+print(hi("Alex"))
